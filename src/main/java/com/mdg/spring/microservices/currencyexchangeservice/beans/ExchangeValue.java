b9f2555c62889fc08c533d4f124c5aa163e10bef
Muralidhar Goud Ediga <muralidhar.g@247.ai>
2020-02-10 10:48:54 +0530

    initial commit

---
+++ src/main/java/com/mdg/spring/microservices/currencyexchangeservice/beans/ExchangeValue.java
@@ -0,0 +1,44 @@
+package com.mdg.spring.microservices.currencyexchangeservice.beans;
+
+import java.math.BigDecimal;
+
+public class ExchangeValue {
+	private long id;
+	private String from;
+	private String to;
+	BigDecimal conversionMultiple;
+	private int port;
+
+	public ExchangeValue(long id, String from, String to, BigDecimal conversionMultiple) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionMultiple = conversionMultiple;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public BigDecimal getConversionMultiple() {
+		return conversionMultiple;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+}
